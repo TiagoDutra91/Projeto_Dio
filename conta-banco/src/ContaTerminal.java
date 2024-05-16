2ba91ee22ae0f8aaec5bdd64e65f4a42a8abf4c1
TiagoDutra91 <pavanati.tiago@gmail.com>
2024-05-15 22:22:56 -0300

    Conta Bancaria

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    
+        System.out.println("Olá, Digite seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, Digite o numero de sua conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, Digite o Numero de Sua Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, Digite o valor que deseja depositar:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome);
+        System.out.println("Obrigado por criar uma conta em nosso banco");
+        System.out.println("Sua Agência é : " + agencia);
+        System.out.println("Seu saldo: R$" + saldo  + "  Já está disponivel para saque.");
+    
+
+
+
+    }
+}
